57db059ddae30e2b050f0d0916ded48d3b333ece
martin <them492017@gmail.com>
2025-03-09 00:26:43 +1100

    Improved binary usage when command line args aren't provided

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,28 @@ use rust_virtual_kvm::server;
 use rust_virtual_kvm::client;
 use rust_virtual_kvm::ui;
 
+const WELCOME_STRING: &str = r#"
+====================================================================
+    Software KVM
+====================================================================
+    In order to have access to keyboard and mouse
+    devices, this binary should be run with root permissions
+====================================================================
+"#;
+
 #[tokio::main]
 async fn main() -> Result<(), DynError> {
+    println!("{WELCOME_STRING}");
     let args: Vec<String> = std::env::args().collect();
 
     if args.contains(&"--server".to_string()) {
-        let a = parse_server_args(args)?;
-        server::server_loop::run(a).await
+        let server_addr = parse_server_args(args)?;
+        server::server_loop::run(server_addr).await
     } else if args.contains(&"--client".to_string()) {
-        let (a1, a2) = parse_client_args(args)?;
-        client::client_loop::run(a1, a2).await
+        let (server_addr, client_addr) = parse_client_args(args)?;
+        client::client_loop::run(server_addr, client_addr).await
     } else {
-        println!("Should run ui");
-        ui::ui::ui();
-        Ok(())
+        ui::ui::ui().await
     }
 }
 
--- src/ui/ui.rs
+++ src/ui/ui.rs
@@ -1,3 +1,47 @@
-pub fn ui() {
-    todo!("Should start ui")
+use std::{io::Write, net::SocketAddr};
+
+use crate::{client::client_loop, common::error::DynError, server::server_loop};
+
+fn get_input() -> String {
+    let _ = std::io::stdout().flush();
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
+
+pub async fn ui() -> Result<(), DynError> {
+    // TODO: Should start egui application
+    print!("Server / Client: ");
+    let chosen = get_input();
+
+    match chosen.to_lowercase().as_str() {
+        "server" => {
+            print!("Server Address: ");
+            let addr = get_input();
+            let server_addr: SocketAddr = addr
+                .as_str()
+                .parse()
+                .expect("Should provide a valid socket address");
+            server_loop::run(server_addr).await?;
+        }
+        "client" => {
+            print!("Server Address: ");
+            let server_addr: SocketAddr = get_input()
+                .as_str()
+                .parse()
+                .expect("Should provide a valid socket address");
+            print!("Client Address: ");
+            let client_addr: SocketAddr = get_input()
+                .as_str()
+                .parse()
+                .expect("Should provide a valid socket address");
+            client_loop::run(server_addr, client_addr).await?;
+        }
+        _ => {
+            println!("Response was '{}'", chosen);
+            panic!("Response should be one of 'server' or 'client'");
+        }
+    }
+
+    Ok(())
 }
